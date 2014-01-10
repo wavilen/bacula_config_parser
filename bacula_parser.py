30eb756bde6f8dcdd1e525d1cf26b1ceb81c8077
Valen Tau <wavilen@gmail.com>
2014-01-10 15:32:08 +0400

    more clean code and fix many problem

--- bacula_parser.py
+++ bacula_parser.py
@@ -1,66 +1,65 @@
 #!/usr/bin/python2
 from pyparsing import *
+import re
+import sys
+
+
+def remove_comment(filepath):
+    comment = re.compile(r'#.*$')
+    empty_line = re.compile(r'^\s*$')
+    file_without_comment = ''
+    with open(filepath, "r") as sources:
+        for line in sources.readlines():
+            file_without_comment += re.sub(
+                empty_line, '', re.sub(comment, '', line)
+            )
+
+    return file_without_comment
 
 
 def baculaParser(dir_config_file):
 
-    LBRACE, RBRACE, EQUALS, HASH = map(Suppress, '{}=#')
+    LBRACE, RBRACE = map(Suppress, '{}')
 
-    comment = HASH + Optional(restOfLine)
     strip = lambda t: t[0].strip(' "')
 
-    SecName = Word(alphanums) + Optional(Suppress(comment))
-    NONEQUALS = "".join(
-        [c for c in srange('[a-zA-Z_]') if c != "="]
-    ) + " \t"
-    NONEQUALSEXT = "".join(
-        [c for c in srange('[a-zA-Z0-9_"./]') if c != "="]
-    ) + " \t"
+    SecName = Word(alphanums)
 
-    lineWithoutComment = restOfLine.setParseAction(
-        lambda t: ((t[0].partition('#'))[0]).strip(' "')
+    rsym = re.compile('[={}]')
+    setList = delimitedList(
+        Word(rsym.sub('', printables) + ' ').setParseAction(strip),
+        delim='='
     )
-
-    keyDefEQ = (
-        Word(NONEQUALS).setParseAction(strip) + EQUALS
-        + Word(NONEQUALSEXT).setParseAction(strip) + EQUALS
-        + lineWithoutComment
-    )
-    keyDefQuote = (
-        Word(NONEQUALS).setParseAction(strip) + EQUALS
-        + Regex(r'"[a-zA-Z0-9.-_]*"').setParseAction(lambda t: t[0].strip('"'))
-    )
-    keyDef = (
-        Word(NONEQUALS).setParseAction(strip) + EQUALS
-        + lineWithoutComment
-    )
-    keyList = delimitedList(keyDefQuote, delim=';')
 
     baculaObject = Forward()
     baculaDef = Forward()
     baculaSec = Forward()
 
-    incFile = lambda t: baculaSec.parseFile(t[0])
+    incSecParse = lambda t: baculaSec.parseString(remove_comment(t[0]))
+    incObjParse = lambda t: baculaObject.parseString(remove_comment(t[0]))
 
-    incDef = (Literal('@').suppress() + restOfLine).setParseAction(incFile)
+    incSec = (Literal('@').suppress() + restOfLine).setParseAction(incSecParse)
+    incObj = (Literal('@').suppress() + restOfLine).setParseAction(incObjParse)
 
     baculaSec << Dict(ZeroOrMore(
-        Group(keyDefEQ | keyList | keyDef | baculaDef) | incDef
+        Group(incSec | baculaDef | setList)
     ))
     baculaDef << SecName + LBRACE + Optional(baculaSec) + RBRACE
-    baculaObject << Dict(ZeroOrMore(Group(baculaDef) | incDef))
+    baculaObject << Dict(ZeroOrMore(Group(baculaDef) | incObj))
 
-    baculaObject.ignore(comment)
-
-    return baculaObject.parseFile(dir_config_file)
+    return baculaObject.parseString(remove_comment(dir_config_file))
 
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser(
-        description='This script parse bacula director config to python dictionary.')
-    parser.add_argument('filename',
-             help='path to filename bacula-dir.conf') 
+    parser = argparse.ArgumentParser(description=(
+        'This script parse bacula director config'
+        ' to python dictionary.'
+    ))
+    parser.add_argument(
+        'filename',
+        help='path to filename bacula-dir.conf'
+    )
     args = parser.parse_args()
     results = baculaParser(args.filename)
     import pprint
